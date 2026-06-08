b4fe55fc43f617aeea5941445bb71e5644571f74
schizo16 <schizo16@github.com>
2026-06-08 11:45:30 +0700

    feat: add 3D animated background with parallax orbs

--- src/components/layout/Shell.tsx
+++ src/components/layout/Shell.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import { Sidebar } from './Sidebar'
 import { TopBar } from './TopBar'
+import { Background3D } from '@/components/shared/Background3D'
 
 export function Shell() {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex min-h-screen relative">
+      <Background3D />
       <Sidebar />
-      <div className="ml-60 flex flex-1 flex-col">
+      <div className="ml-60 flex flex-1 flex-col relative z-10">
         <TopBar />
         <main className="mt-12 flex-1 p-6">
           <Outlet />
